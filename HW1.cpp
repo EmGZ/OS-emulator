77476cf1911510abf72c8adf6678083624490a78
Erika <mary_erika_culala@dlsu.edu.ph>
2025-05-13 22:55:58 +0800

    removed cliapp

--- HW1.cpp
+++ HW1.cpp
@@ -1,7 +1,24 @@
-#include "CLIApp.h"
+#include "Console.h"
+#include "CommandProcessor.h"
+#include <iostream>
 
 int main() {
-    CLIApp app;
-    app.run();
+    Console console;
+    CommandProcessor processor;
+    std::string input;
+    console.printDefaultDisplay();
+
+    while (true) {
+        std::cout << "Enter a command: ";
+        console.setColor(7);  // Reset to white
+
+        std::getline(std::cin, input);
+
+        if (!input.empty()) {
+            bool keepRunning = processor.executeCommand(input, console);
+            if (!keepRunning)
+                break;
+        }
+    }
     return 0;
 }
